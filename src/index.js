d87ba22d43a5bf88ab8231a9558357faa2613b11
ivtaj <ivan.voytseshyn@tajamar365.com>
2025-03-25 20:07:13 +0100

    c

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-import React from 'react';
-
 function App() {
   const handleClick = () => {
     alert('¡El botón se ha clickeado!');
@@ -37,9 +28,13 @@ function App() {
   );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 export default App;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
