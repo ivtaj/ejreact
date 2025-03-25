cd5222eff01406a8827bdb27e4ea23f35f3db6dc
ivtaj <ivan.voytseshyn@tajamar365.com>
2025-03-25 20:03:27 +0100

    prueba

--- src/index.js
+++ src/index.js
@@ -11,6 +11,34 @@ root.render(
   </React.StrictMode>
 );
 
+import React from 'react';
+
+function App() {
+  const handleClick = () => {
+    alert('¡El botón se ha clickeado!');
+  };
+
+  return (
+    <div className="App">
+      <button 
+        onClick={handleClick}
+        style={{
+          padding: '10px 20px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }}
+      >
+        Haz clic aquí
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
